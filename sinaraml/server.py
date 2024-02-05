1e2ca441e5114144384067a86040fb1626e927c9
MiXaiLL76 <mike.milos@yandex.ru>
2024-02-05 18:28:52 +0300

    remove ssh feature

--- sinaraml/server.py
+++ sinaraml/server.py
@@ -109,11 +109,6 @@ class SinaraServer:
             help="Number of CPU cores to use for server container (default: %(default)s)",
         )
         SinaraServer.create_parser.add_argument(
-          "--ssh", 
-          action=argparse.BooleanOptionalAction, 
-          help='Connect ~/.ssh folder to server container'
-        )
-        SinaraServer.create_parser.add_argument(
             "--jovyanRootPath",
             type=str,
             help="Path to parent folder for data, work and tmp (only used in basic mode with createFolders=y)",
@@ -340,9 +335,6 @@ class SinaraServer:
         elif args.runMode == "b":
             docker_volumes = SinaraServer._prepare_basic_mode(args)
 
-        if args.ssh:
-            docker_volumes += SinaraServer._prepare_ssh_volume()
-        
         server_cmd = "start-notebook.sh --ip=0.0.0.0 --port=8888 --NotebookApp.default_url=/lab --ServerApp.allow_password_change=False"
         if args.insecure:
             server_cmd = (
@@ -394,17 +386,7 @@ class SinaraServer:
             f"{work_volume}:/home/jovyan/work",
             f"{tmp_volume}:/tmp",
         ]
-      
-    @staticmethod
-    def _prepare_ssh_volume():
-        ssh_volume = os.path.join(os.path.expanduser('~'), ".ssh")
-        ssh_folders = []
-        
-        if os.path.isdir(ssh_volume):
-            ssh_folders.append(f"{ssh_volume}:/home/jovyan/.ssh")
-        
-        return ssh_folders
-        
+
     @staticmethod
     def _prepare_basic_mode(args):
         if args.createFolders == "y":
