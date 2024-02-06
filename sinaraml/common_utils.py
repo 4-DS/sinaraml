4318f4f011de4d6f03117a112ab85b74aa4688d3
ExcaliburKG <akagorbunov@ya.ru>
2024-02-06 10:36:49 +0300

    Added model profiles support

--- sinaraml/common_utils.py
+++ sinaraml/common_utils.py
@@ -4,6 +4,28 @@ import urllib
 import time
 import socket
 import os
+import json
+
+def get_bentoservice_profile_name(bentoservice_dir):
+    profile = None
+    profile_file = os.path.join(bentoservice_dir, 'bentoservice_profile.json')
+    if os.path.isfile(profile_file):
+        with open(profile_file, 'r+') as f:
+            profile_data = json.load(f)
+            profile = profile_data['bentoservice_profile']['name']
+    return profile
+
+def replace_bentoservice_model_server_image(dockerfile_path, model_server_image):
+    insert_marker = 'from '
+    with open(dockerfile_path, 'r+') as docker_file:
+        dockerfile_content = docker_file.readlines()
+        marker_index = [idx for idx, s in enumerate(dockerfile_content) if insert_marker in s.lower()][0]
+        insert_index = marker_index + 1
+        del dockerfile_content[0]
+        dockerfile_content.insert(0, f"FROM {model_server_image}\n")
+        docker_file.seek(0)
+        docker_file.writelines(dockerfile_content)
+        docker_file.truncate()
 
 def compute_md5(file_name):
     hash_md5 = hashlib.md5()
--- sinaraml/model.py
+++ sinaraml/model.py
@@ -13,7 +13,13 @@ from .docker_utils import docker_container_create, \
                           docker_copy_from_container, \
                           docker_build_image
 
-from .common_utils import compute_md5, get_expanded_path
+from .common_utils import compute_md5, get_expanded_path, replace_bentoservice_model_server_image, get_bentoservice_profile_name
+
+bentoservice_profiles_supported = {
+    'SinaraOnnxBentoService': 'onnx',
+    'SinaraPytorchBentoService': 'pytorch',
+    'SinaraBinaryBentoService': 'binary'
+}
 
 class SinaraModel():
 
@@ -105,6 +111,8 @@ class SinaraModel():
         model_zip_path = bentoservice_cache_dir / "model.zip"
         success_file_path = bentoservice_cache_dir / "_SUCCESS"
         save_info_path = bentoservice_cache_dir / "save_info.txt"
+        bentoservice_dockerfile_path = bentoservice_cache_dir / "Dockerfile"
+
 
         with zipfile.ZipFile(model_zip_path, 'r') as model_zip:
             model_zip.extractall(path=bentoservice_cache_dir)
@@ -116,6 +124,16 @@ class SinaraModel():
         model_image_name_full = f"{args.dockerRegistry}/{model_image_name}:{model_image_tag}"
 
         SinaraModel.save_extra_info(bentoservice_cache_dir, model_image_name_full)
+
+        bentoservice_profile = get_bentoservice_profile_name(bentoservice_cache_dir)
+        if bentoservice_profile:
+            if bentoservice_profile not in bentoservice_profiles_supported.keys():
+                raise Exception(f'Unsupported bentoservice profile "{bentoservice_profile}" in bentoservice found. Supported: {", ".join(bentoservice_profiles_supported.keys())}')
+            
+            print(f'Using bentoservice profile: {bentoservice_profile}')
+
+            if bentoservice_profile == 'SinaraOnnxBentoService':
+                replace_bentoservice_model_server_image(bentoservice_dockerfile_path, "buslovaev/sinara-onnx-model-server")
 
         print(f"Building model image {model_image_name_full}")
         docker_build_image(path=str(bentoservice_cache_dir), tag=model_image_name_full, pull=True, forcerm=True, rm=True, quiet=False)
